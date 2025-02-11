f60087fec94c60d85216c08e816f218a123656d6
Isa Heydaroghlu <heyderovisa777@gmail.com>
2025-02-11 11:27:24 +0400

    @EqualsAndHashCode(callSuper = false)

--- src/main/java/ProjectMS/dto/cardDtos/postCardDto.java
+++ src/main/java/ProjectMS/dto/cardDtos/postCardDto.java
@@ -1,8 +1,10 @@
 package ProjectMS.dto.cardDtos;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class postCardDto {
     String title;
     String description;
--- src/main/java/ProjectMS/dto/cardDtos/putCardDto.java
+++ src/main/java/ProjectMS/dto/cardDtos/putCardDto.java
@@ -2,8 +2,10 @@ package ProjectMS.dto.cardDtos;
 
 import ProjectMS.dto.baseDtos.baseDto;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class putCardDto extends baseDto {
     String title;
     String description;
