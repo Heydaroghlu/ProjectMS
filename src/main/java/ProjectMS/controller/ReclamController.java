b79783a44965e3c009319f799b6a8d4b9fea2280
Isa Heydaroghlu <heyderovisa777@gmail.com>
2025-02-11 11:29:38 +0400

    ReclamEndpoint

--- src/main/java/ProjectMS/controller/ReclamController.java
+++ src/main/java/ProjectMS/controller/ReclamController.java
@@ -2,7 +2,18 @@ package ProjectMS.controller;
 
 import ProjectMS.model.BaseEntity;
 import ProjectMS.model.Reclam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/reclam")
 public class ReclamController {
-
+    @GetMapping()
+    public ResponseEntity<String> getAllReclam() {
+        return ResponseEntity.ok("All Reclam");
+    }
 }
